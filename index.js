257d05d3eafa3108b84c54ee2f72e1654e8beef4
loolys <pierre.liljekvist@outlook.com>
2016-12-17 08:00:43 +0100

    Used express Router to get the url passed and added a url validator to check if the url is valid

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const validUrl = require('valid-url');
 
 // database setup
 const url = process.env.MONGOLAB_URI;
@@ -10,9 +11,32 @@ db.once('open', function() {
   console.log('connected to db');
 });
 
-const app = express();
+// Schema
+const urlSchema = new mongoose.Schema({
+  original_url: String,
+  short_url: String,
+});
 
-app.get('/', function(req, res) {
+const UrlModel = mongoose.model('Url', urlSchema);
+// let itemOne = UrlModel({
+//   original_url: "http://www.google.com",
+//   short_url: "http://bitsi.ze/2",
+// }).save(function(err) {
+//   if (err) throw err;
+//   console.log("saved");
+// });
+
+const app = express();
+const router = express.Router();
+app.use(router);
+
+router.get('/new/:url*', function(req, res, next) {
+  let passed_url = req.params.url + req.params[0];
+  if (validUrl.isUri(passed_url)){
+    console.log("valid url");
+  } else {
+    console.log("Invalid url");
+  }
   res.end("hello");
 });
 
